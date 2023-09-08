f5df4d2905fc4cfb9a466a69f1f2f91abba5c0d7
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-09-08 08:19:39 +0100

    Attempted to integrate firebase

--- todo_list_app/lib/main.dart
+++ todo_list_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_app/add_todo.dart';
@@ -6,7 +7,11 @@ import 'package:todo_list_app/todo_home.dart';
 import 'package:todo_list_app/todo_provider.dart';
 import 'package:todo_list_app/view_todo.dart';
 
-void main() => runApp(const TodoApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const TodoApp());
+}
 
 class TodoApp extends StatelessWidget{
   const TodoApp({super.key});
